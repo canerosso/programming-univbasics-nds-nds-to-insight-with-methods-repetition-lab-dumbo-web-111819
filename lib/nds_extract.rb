42a9b982b4bd1f1daa1d2f76fcc6d6331a3641ba
Cane Rosso <astor@usa.com>
2019-11-11 13:40:36 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -48,12 +48,9 @@ def total_gross(source)
   # Visit each key (i.e. director name), look up the value in the hash
   # returned by directors_totals, and add it to a running total. When done,
   # return the total
-  
- 
+   dir_names = list_of_directors(source)
   i = 0
-
   total = 0
-
   while i < dir_names.length do
     dir_name = dir_names[i]
     total += dir_to_earnings_hash[dir_name]
